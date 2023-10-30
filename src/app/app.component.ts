600d3399878c38a08f6fd5ecd3984396fa13618b
DumitruIonutRazvan <dumitruionutrazvan92@gmail.com>
2023-10-31 01:03:40 +0200

    fetching products data from api and render them

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from './services/product.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'products-page';
+  productsArray: any[] = [];
+
+  constructor(private productSrv: ProductService){
+
+  }
+
+  ngOnInit(): void {
+    this.loadProducts();
+
+  }
+
+loadProducts() {
+  this.productSrv.getAllProducts().subscribe((Res: any)=> {
+    this.productsArray = Res.data;
+    })
+}
+
 }
